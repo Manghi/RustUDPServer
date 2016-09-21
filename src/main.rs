f8e7d71f1b13f4101bb760b5af188f3c3e19dfba
Manghi <amunimanghi@example.com>
2016-09-20 23:20:59 -0700

    Implemented Serialized encoding/decoding of UDP Packet
    - Uses rustc_serialize and bincode crates :)
    --> Outsources unsafe-ness to crates
    - Removed C representation
    - Implemented Diplay & Debug traits for Packet Struct

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,83 @@
+extern crate bincode;
+extern crate rustc_serialize;
+
 use std::thread;
 use std::time;
 use std::net;
-use std::slice;
 use std::mem;
+use std::fmt;
+
 
 mod nethelper;
 
-#[repr(C, packed)]
+#[repr(packed)]
+#[derive(RustcEncodable, RustcDecodable, PartialEq)]
 pub struct UDPHeader {
-    pub signature: [u8; 4],
+    pub signature: [char; 4],
 }
 
-#[repr(C, packed)]
+#[repr(packed)]
+#[derive(RustcEncodable, RustcDecodable, PartialEq)]
 pub struct UDPData {
     pub numerical: [u8; 10],
     pub textual: [char;10],
+    pub vector: Vec<u32>,
 }
 
-#[repr(C, packed)]
+#[repr(packed)]
+#[derive(RustcEncodable, RustcDecodable, PartialEq)]
 pub struct Packet {
     pub header: UDPHeader,
     pub data: UDPData,
 }
+
+impl fmt::Debug for UDPHeader {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self) 
+    }
+}
+
+impl fmt::Debug for UDPData {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self) 
+    }
+}
+
+impl fmt::Debug for Packet {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", self) 
+    }
+}
+
+impl fmt::Display for UDPHeader {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Signature:{:?}", self.signature)
+    }
+}
+
+impl fmt::Display for UDPData {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Numerical:{:?}\n  Textual:{:?}\n  Vector:{:?}", self.numerical, self.textual, self.vector)
+    }
+}
+
+impl fmt::Display for Packet {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "\nPacket:\n  {:?}\n  {:?}\n", self.header, self.data)
+    }
+}
+
+trait MyLen {
+    fn len(&self) -> usize;
+}
+
+impl MyLen for Packet {
+    // We already have the number of iterations, so we can use it directly.
+    fn len(&self) -> usize {
+        mem::size_of::<Packet>()
+    }
+}
+
 
 pub fn main()
 {
@@ -34,14 +90,16 @@ pub fn main()
     //let message: Vec<u8> = vec![1;10];
 
     let structmessage = Packet {
-        header: UDPHeader { signature: [82, 85, 83, 84] },
-        data: UDPData { numerical: [1;10], textual: ['a','b','c','d','e','f','g','h','i','j'] },
+        header: UDPHeader { signature: ['L', 'I', 'F', 'E'] },
+        data: UDPData { numerical: [1;10], textual: ['a','b','c','d','e','f','g','h','i','j'], vector: vec![8675309, 10000, 2^32-1] },
     };
+
+    println!("Message size: {} Bytes", structmessage.len());
 
     // give the thread 3s to open the socket
     thread::sleep(time::Duration::from_millis(3000));
 
-        unsafe {
+/*        unsafe {
         //let sentmessage1: Vec<u32> = std::mem::transmute(structmessage);
         //let sentmessage: Vec<u8> = std::mem::transmute(sentmessage1);
 
@@ -49,15 +107,21 @@ pub fn main()
         let p: *const u8 = p as *const u8;  // convert between pointer types
         let sentmessage1: &[u8] =  slice::from_raw_parts(p, mem::size_of::<Packet>());
         let sentmessage = Vec::from(sentmessage1);
-
-        nethelper::send_message(net::SocketAddr::V4(send_addr), net::SocketAddr::V4(listen_addr), sentmessage);
+*/
+        {
+            let sentmsg_encoded: Vec<u8> = bincode::rustc_serialize::encode(&structmessage, bincode::SizeLimit::Infinite).unwrap();
+            nethelper::send_message(net::SocketAddr::V4(send_addr), net::SocketAddr::V4(listen_addr), sentmsg_encoded);
         }
 
     println!("Waiting");
 
-    let received = future.join().unwrap();
-    println!("Got {} bytes", received.len());
-    println!("Data: {:?}", received);
-    assert_eq!(54, received.len());
-    assert_eq!(82, received[0]);
+    let rcvdmsg = future.join().unwrap();
+    
+    let decoded: Packet = bincode::rustc_serialize::decode(&rcvdmsg[..]).unwrap();
+
+    
+    println!("Got {} bytes", rcvdmsg.len());
+    println!("{:?}", decoded);
+    assert_eq!(structmessage.len(), decoded.len());
+    assert_eq!(decoded, structmessage);
 }
